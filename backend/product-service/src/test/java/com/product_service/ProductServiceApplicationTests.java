8280c5221057bdb6ec8b57f356498f7c6dd02c9f
sabdulla <sarakhalaf23012@gmail.com>
2026-02-04 21:41:47 +0300

    @EmbeddedKafka

--- backend/product-service/src/test/java/com/product_service/ProductServiceApplicationTests.java
+++ backend/product-service/src/test/java/com/product_service/ProductServiceApplicationTests.java
@@ -3,9 +3,11 @@ package com.product_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest(classes = ProductServiceApplication.class)
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:0", "port=0"})
 class ProductServiceApplicationTests {
 
     @Test
--- backend/user-service/src/test/java/com/user-service/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/user-service/UserServiceApplicationTests.java
@@ -3,9 +3,11 @@ package com.user_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest(classes = UserServiceApplication.class)
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:0", "port=0"})
 class UserServiceApplicationTests {
 
     @Test
