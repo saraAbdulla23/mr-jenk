084ce583478305b0582e5b5bb261debb888a7bec
sabdulla <sarakhalaf23012@gmail.com>
2026-02-04 22:24:09 +0300

    updated

--- backend/media-service/src/test/java/backend/media_service/MediaServiceApplicationTests.java
+++ backend/media-service/src/test/java/backend/media_service/MediaServiceApplicationTests.java
@@ -4,10 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootTest(classes = MediaServiceApplication.class)
 @ActiveProfiles("test")
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:0", "port=0"})
+@EmbeddedKafka(
+    partitions = 1,
+    brokerProperties = {
+        "listeners=PLAINTEXT://localhost:0",
+        "port=0"
+    }
+)
+@EnableAutoConfiguration(exclude = {
+    SecurityAutoConfiguration.class,
+    UserDetailsServiceAutoConfiguration.class
+})
 class MediaServiceApplicationTests {
 
     @Test
